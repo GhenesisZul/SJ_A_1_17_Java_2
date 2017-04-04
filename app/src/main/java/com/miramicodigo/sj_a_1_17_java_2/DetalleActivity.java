47aca764eba3d156c86f9390ecb3035ae8370f95
Gustavo <lizarraga.gux@gmail.com>
2017-04-04 15:26:54 -0400

    Commit inicial

---
+++ app/src/main/java/com/miramicodigo/sj_a_1_17_java_2/DetalleActivity.java
@@ -0,0 +1,20 @@
+package com.miramicodigo.sj_a_1_17_java_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetalleActivity extends AppCompatActivity {
+
+    /**
+     * @author Gustavo Lizarraga
+     * @date 04-04-17
+     * */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+
+    }
+}
---
+++ app/src/main/java/com/miramicodigo/sj_a_1_17_java_2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.miramicodigo.sj_a_1_17_java_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * @author Gustavo Lizarraga
+     * @date 04-04-17
+     * */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
